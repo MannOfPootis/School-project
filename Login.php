04dee83f4acef6de42fd6e0016dfbb3cbd60c0d0
MannOfPootis <99479995+MannOfPootis@users.noreply.github.com>
2022-09-07 12:02:11 +0200

    databseSUS

--- Login.php
+++ Login.php
@@ -1,45 +1,47 @@
 <h1> this is the login form </h1>
-<form action="" method="post"><b>
-First Name: <input type="text" name="name"><br>
-Last Name:  <input type="text" name="surname"><br>
-Username:   <input type="text" name="username"><br>
-Password:   <input type="password" name="password"id ="password" onchange="checkok()"><br>
-Password again<input type = "password" name="password2" id ="password1" onchange="checkok()">
-    </b>
-<input type="submit" >
-<div id ="problem"></div>
 <?php
-            if(array_key_exists( "name",$_POST)&&
-            array_key_exists( "surname",$_POST)&&
-            array_key_exists( "username",$_POST)&&
-            array_key_exists( "password",$_POST)&&
-            array_key_exists( "password2",$_POST) 
-        
-        
-            ){
-            if($_POST["password"]==$_POST["password2"])
-            {
-                $firstname=$_POST["name"];
-                $lastname=$_POST["surname"];
-                $username=$_POST["username"];
-                $password=password_hash($_POST["password"],PASSWORD_DEFAULT);
-                if($conn->query("select username from user where username='$username'")->num_rows>0)
-                {
-                    echo "<h1 style ='color: red;'>Username already taken</h1>";
-                }
-                else
-                {
-                $sql = "INSERT INTO user(name  ,surname  ,username   ,password)
-                values('$firstname','$lastname','$username','$password')";
-                if ($conn->query($sql)) {
-                    echo "<h1 style ='color: green;'>New record created successfully</h1>";
-                  } else {
-                    echo "Error: " . $sql . "<br>" . $conn->error;
-                  }
-                }
-                $conn->close();
-            }
+session_start();
+if(
+    array_key_exists( "username",$_POST)&&
+    array_key_exists( "password",$_POST)
+    )
+    {
+        $username=$_POST["username"];
+        $password=$_POST["password"];
+        $gotten_password=sqli_takefirst($conn->query("SELECT password from user where username ='$username'"));
+        if(password_verify($password, $gotten_password))
+        {
+
+            $_SESSION["username"]=$username;
+            header('Refresh: 0');
+
         }
-        ?>
+        else{
+            echo "<p>bad login boyo</p>";
+            session_destroy();
+            
+
+        }
+    }
+?>
+
+<br>
+
+<?php
+if( !array_key_exists( "username",$_SESSION))
+{
+echo'
+<form action="" method="post">
+
+Please log in<br><br>
+<div class="borderp">
+username:<br> <input type="text" name="username"><br>
+password:<br>   <input type="password" name="password"><br>
+<input type="submit" value="login">
 </div>
 </form>
+</h2>
+
+';}
+
+?>
--- MainPage.php
+++ MainPage.php
@@ -1,7 +1,10 @@
 <h1>ligma test</h1>
 <?php
+include "Methods.php";
 include "config.php";
 include "Login.php";
+include "Signin.php";
+
 
 ?>
 <div>ligma success</div> a
