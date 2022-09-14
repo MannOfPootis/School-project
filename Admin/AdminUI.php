0ae8ebc61ec45631dfb037996d8a1b47beb36021
MannOfPootis <99479995+MannOfPootis@users.noreply.github.com>
2022-09-14 11:39:06 +0200

    Admin UI
    Made ADmin UI work but ugly

--- Admin/AdminUI.php
+++ Admin/AdminUI.php
@@ -1,9 +1,30 @@
-<?php
+
+
+   
+       <form action ="" method = "post">
+        <?php
 include 'AdminConfig.php';
+foreach($_POST as $name => $answer)
+{
+    echo "<h1>$name $answer</h1> <br> ";
+    switch ($answer)
+    {
+        case "null":
+            break;
+        case "yes":
+            $conn->query("UPDATE from user set permission =claim where username = '$name'");
+            break;
+        case "no":
+            $conn->query("DELETE from user where username = '$name'");
+            break;
+    }
+}
 
 
-$allDif = $conn->query("SELECT * FROM user  WHERE claim != permission AND claim =3");
+
+$allDif = $conn->query("SELECT * FROM user  WHERE claim > permission /*AND claim =3*/");
 while ($allDifArray=mysqli_fetch_array($allDif)){
+    $claimUsername=$allDifArray["username"];
     $name=$allDifArray["name"];
     $surname= $allDifArray["surname"];
     $claim=$allDifArray['Claim'];
@@ -20,16 +41,28 @@ while ($allDifArray=mysqli_fetch_array($allDif)){
             echo "an admin ";
             break;
     }
-    echo"their surname is $surname";
-    echo'<form method = "post">
-    <select value="no '.$username.'" >no</select>
-    <select value="yes '.$username.'" >esy</select>
-</form>';
+    echo"their surname is $surname  $claimUsername ";
+    echo'
+
+    <select name = '.$claimUsername.' > 
+        <option value = "null"> null</option>
+        <option value="no" >no</option>
+        <option value="yes" >yes</option>
+    </select>
+    
+    <br>
+   
+
+';
 }
+
 ?>
-<p>can you confirm ?</p>
+<input type ="submit">
+    </form>
+
+<!--<p>can you confirm ?</p>
 <form method = "post">
     <select value="no" >no</select>
     <select value="yes" >esy</select>
-</form>
+</form>-->
 <h1>you are now the admin</h1>
