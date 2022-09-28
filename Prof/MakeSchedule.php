0ed785f62dd8bf62d9bf968bee6f0a72bccb6b9d
zackstormberry <zackstormberry@gmail.com>
2022-09-28 12:06:06 +0200

    bit of work

--- Prof/MakeSchedule.php
+++ Prof/MakeSchedule.php
@@ -2,6 +2,18 @@
 <table>
 <tr><td>  </td><td>pon</td><td>tor</td><td>sre</td><td>čet</td><td>pet</td></tr>
 <?php
+if(
+    isset($_POST["1,1,13"])||true
+){
+    $endArray;
+    foreach($_POST as $val ){
+        $thingArray=explode(",", $val,3);
+        $thot=$thingArray['0'];
+        echo "$thot";
+    }
+    echo "$thot";
+
+}
 
 for ($hour = 0; $hour <= 8; $hour++){
     echo"<tr>sds";
@@ -9,14 +21,22 @@ for ($hour = 0; $hour <= 8; $hour++){
         {
 
             if($day>0){
-            echo"<td><select name = 'subject'>";
-            echo"<option value ='$day,$hour,subjectid' >sdad</option>";
+            echo"<td><select name = '$day,$hour,subject'>";
+            echo"<option value ='$subjectid' >sdad</option>";//add this once im back on my pc
             echo"</select>";
             
             
             
-            echo"<select name='teach' >";
-            echo"<option value ='$day,$hour,teachtid'> sdsd </option>";
+            echo"<select name='$day,$hour,teach' >";
+            $teachers = $conn->query("select * from user where permission = 2");
+            while ($teachArray=$teachers->fetch_assoc())
+            {
+                $teachid=$teachArray["ID"];
+                $teachName=$teachArray["name"]." ".$teachArray["surname"];
+                echo"<option value ='$teachtid'> $teachName </option>";
+                
+            }
+            //add this too
             echo" </select></td>";
 
             }
