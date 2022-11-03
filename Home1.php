458f12546560caad0e4856595e7c9096f6707942
zackstormberry <zackstormberry@gmail.com>
2022-11-03 11:44:30 +0100

    table adjustment

--- Home1.php
+++ Home1.php
@@ -49,10 +49,16 @@
 
  			<div class="home_right">
 
-			<form action="functions.php" method="POST">
+			<form action="functions.php" method="POST" >
+
+
+
+
  			<div style="border-bottom-style: solid;"> YOUR TIMETABLE </div>
+
             <div class="info" style="text-align: center; width: 500px; height: 400px;">      
-			<table>
+			
+			<table >
 				<tr><td>  </td><td>pon</td><td>tor</td><td>sre</td><td>čet</td><td>pet</td></tr>
 			<?php
 				for ($hour = 0; $hour <= 8; $hour++){
@@ -61,7 +67,7 @@
 						{
 				
 							if($day>0){
-							echo"<td><p id = '$day,$hour,subject'>h</p>";
+							echo"<td><p style='margin: 1px' id = '$day,$hour,subject'>h</p>";
 							
 				
 							}
@@ -86,7 +92,7 @@
 					}
 				?>	
 				</table>
-              
+			
 
             </div>
 			
