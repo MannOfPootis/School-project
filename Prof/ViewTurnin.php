e7d1954c2ca486fe8ae1738e15bad1f5f1c884b0
MannOfPootis <janez.domini@gmail.com>
2022-11-08 00:42:47 +0100

    man I am smolbraining

--- Prof/ViewTurnin.php
+++ Prof/ViewTurnin.php
@@ -1,22 +1,36 @@
 <?php
 $dir = "../uploads/";
 $un = $_SESSION["username"];
-$allAssSQLI=$conn->query("SELECT distinct * FROM ASSIGNMENT INNER join user on(assignment.teacher = user.id) WHERE user.username = '$un'");
+$allAssSQLI=$conn->query("SELECT distinct ASSIGNMENT.* FROM ASSIGNMENT INNER join user on(assignment.teacher = user.id) WHERE user.username = '$un'");
 while($allAss=$allAssSQLI->fetch_assoc())
 {
     $title=$allAss["title"];
+    $id = $allAss["ID"];
     $tempDir=$dir.$title."turnin";
-    echo"$title";
+    if(isset($_POST["remove$id"]))
+    {
+        unlink("../uploads/$title");
+        echo"../uploads/$title";
+        //unlink("../uploads/$title"."turnin");
+        if($conn->query("DELETE FROM ASSIGNMENT WHERE ID = $id")){}else{
+            echo "Error: " . "DELETE FROM ASSIGNMENT WHERE ID = $id" . "<br>" . $conn->error;
+        }
+    }else{
+    echo"<h1>$title</h1> <br><ul>";
+    echo "<form method ='post' ><input name = 'remove$id' type='submit' value ='remove'></form>";}
+    
+ 
     if (is_dir($tempDir))
     {
         if ($dh = opendir($tempDir)){
             while (($file = readdir($dh)) !== false)
             {
-                echo "filename:<a href ='http://localhost/school-project/uploads/$title"."turnin"."/$file' download>" . $file . "<a><br>";
+                echo "<li>filename:<a href ='http://localhost/school-project/uploads/$title"."turnin"."/$file' download>" . $file . "<a><br></li>";
             }
         closedir($dh);
         }
     }
+    echo "</ul>";
 
 }
 // Open a directory, and read its contents
