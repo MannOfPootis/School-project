66640be9662bef2bf1d1d39810226e3bdb89527e
MannOfPootis <janez.domini@gmail.com>
2022-11-07 20:38:06 +0100

    no errorx this time

--- Pupil/TurnIn.php
+++ Pupil/TurnIn.php
@@ -26,14 +26,15 @@ if($bruhmoment = $conn->query("select * from user where username = '$un'")->fetc
 }
 
 $target_dir = "uploads/".$title."turnin/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-$target_file = $target_dir."$name"."$surname-" . basename($_FILES["fileToUpload"]["name"]);
+
 
 //echo"$imageFileType";
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit$idAss"])) {
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$target_file = $target_dir."$name"."$surname-" . basename($_FILES["fileToUpload"]["name"]);
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
     echo "File is an image - " . $check["mime"] . ".";
