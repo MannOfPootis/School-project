8ba97f935fb61760d8505e1538aa9015f95ee68d
MannOfPootis <janez.domini@gmail.com>
2022-11-07 17:57:39 +0100

    update
    update

--- Prof/GiveAssignment.php
+++ Prof/GiveAssignment.php
@@ -1,35 +1,84 @@
-<form method ="post">
-
-<input type ="text" name = "title">
-<input type="file" name="fileToUpload" id="fileToUpload">
-<input type="date" name ="due">
-<input type="submit">
 
 
-</form> 
+<!DOCTYPE html>
+<html>
+<body>
+  <?php include "../header.php";?>
+
+<form action="" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  
+  <input type = "date" name = "due">
+  for subject
+  <select name = "subject">
+    <option></option>
+    <?php
+      $un = $_SESSION["username"];
+    
+      //echo "$un";
+      
+  	  $teachesSQLI= $conn->query("SELECT distinct SUBJECTS.* FROM SUBJECTS INNER JOIN SCHEDULE ON (subjects.id = schedule.subject) inner join user on(user.id = schedule.teacher) where user.username = '$un'");
+      //unset($un);
+      while ($teaches = $teachesSQLI->fetch_assoc()){
+        $a = $teaches["ID"];
+        $b =$teaches["name"];
+        echo"<option value='$a'>$b</option>";
+      }
+    ?>
+  </select>
+  for class
+  <select name = "class">
+    <option value ="0">all</option>
+    <?php
+      $un = $_SESSION["username"];
+      
+      //echo "$un";
+  	  $teachesSQLI= $conn->query("SELECT distinct class.* FROM class INNER JOIN SCHEDULE ON (class.id = schedule.class) inner join user on(user.id = schedule.teacher) where user.username = '$un'");//
+      //unset($un);
+      while ($teaches = $teachesSQLI->fetch_assoc()){
+        $a = $teaches["id"];
+        $b =$teaches["name"];
+        echo"<option value='$a'>$b</option>";
+      }
+    ?>
+  </select>
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+aerararasea
+</body>sadeew
+</html>
+
 <?php
 
 
-
-
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    
-$target_dir = "../uploads";
+$target_dir = "../uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"]) && isset($_POST["due"]) && isset($_POST["due"]) && isset($_POST["class"])&& isset($_POST["subject"])) {
+
+  $title = $_POST["title"];
+  $due = $_POST["due"];
+  $class = $_POST["class"];
+  $subject = $_POST["subject"];
+  $teachID= sqli_takefirst( $conn->query("select id from user where username = '$un'"));
+
+  $conn->query("INSERT INTO ASSIGNMENT(TEACHER,SUBJECT, DUE, TITLE, CLASS) VALUES('$teachID', '$subject','$due','". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"]))."','$class')");
+
+
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
     echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
     echo "File is not an image.";
-    $uploadOk = 0;
+    $uploadOk = 1;
   }
   if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
+    echo "Sorry, file already exists. we are replacing it ";
+    $uploadOk = 1;
   } 
   if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
@@ -42,4 +91,7 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
     }
   }
 }
+include "../footer.php";
 ?>
+
+
--- Pupil/FindClasses.php
+++ Pupil/FindClasses.php
@@ -3,19 +3,26 @@ list of subjects you atend
 $UN = $_SESSION["username"];
 //$idClass = sqli_takefirst($conn->query("SELECT ID_class FROM ATENDS INNER JOIN USER on(atends.user=user.ID) where user.username ='$UN'"));
 
-$classesSQLI = $conn->query("select distinct subjects.name as subjectname from subjects inner join schedule on (true) inner join class on (class.id = schedule.class) inner join atends on (atends.ID_class = class.id) INNER JOIN USER on(atends.user=user.ID) inner join extra_subjects on ( extra_subjects.ID_user=user.ID or subjects.id = schedule.subject)  where user.username = '$UN'");
+$classesSQLI = $conn->query("select distinct subjects.name as subjectname,subjects.id as gigachad from subjects inner join schedule on (true) inner join class on (class.id = schedule.class) inner join atends on (atends.ID_class = class.id) INNER JOIN USER on(atends.user=user.ID) inner join extra_subjects on ( extra_subjects.ID_user=user.ID or subjects.id = schedule.subject)  where user.username = '$UN'");
                              
 unset($UN);
 while ($classes = $classesSQLI->fetch_assoc()){
     $lul =$classes["subjectname"];
-    echo" $lul ";
+    $gigachad= $classes["gigachad"];
+    echo"<a href='http://localhost/school-project/Home1.php?subject=$gigachad'> $lul </a>";
     //echo"$UN";
 
 }
-unset($UN);
-/*where atends.user = '$UN'*/
-/*inner join extra_subjects on ( extra_subjects.ID_user=user.ID or subjects.id = schedule.subject)*/
-/**/
-/*"select distinct subjects.name as subjectname from subjects inner join schedule on (subjects.id = schedule.subject) inner join class on (class.id = schedule.class) inner join atends on (atends.ID_class = class.id) INNER JOIN USER on(atends.user=user.ID)  where user.username = '$UN'");*/
-/*select* from user inner join extra_subjects on ( extra_subjects.ID_user=user.ID)*/
+/*if(isset($_GET['subject']))
+{
+    $subject =  $_GET['subject'];
+    $materialSQLI =$conn->query("select assignment.* from assignment inner join user on (user.id = assignment.teacher) inner join subjects on (subjects.ID =assignment.subject) where subjects.ID = '$subject' ");
+    while ($material = $materialSQLI.fetch_assoc())
+    {
+
+    }
+
+
+}*/
+
 ?>
